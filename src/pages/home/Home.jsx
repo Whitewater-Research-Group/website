890bf4d41ad2c2d3b29ae5c246c9456880be8c5f
obuhdaniel <danielobuh05@gmail.com>
2024-10-24 23:25:23 +0100

    update: added registration links for the buttons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -76,7 +76,7 @@ function Home() {
             <img src={Calender} alt="Event Calendar" className="h-10 w-10" />
             <span className="text-white text-lg">5th November 2024</span>
             <a
-              href="#contact"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdR7yZDcKB87xumPFnOirR8bKJJzDVyEh9d52u6u0K4hCqtcQ/viewform?pli=1"
               className="text-white text-base font-medium bg-turquoiseBlue rounded-full px-4 py-2"
             >
               Register
@@ -224,7 +224,7 @@ function Home() {
 
     {/* Call to action button */}
     <div className="mt-8">
-      <a href="#contact" className="bg-turquoiseBlue hover:bg-cyan-500 text-white font-semibold text-lg lg:text-xl rounded-full px-6 py-3 transition duration-300">
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSdR7yZDcKB87xumPFnOirR8bKJJzDVyEh9d52u6u0K4hCqtcQ/viewform?pli=1" className="bg-turquoiseBlue hover:bg-cyan-500 text-white font-semibold text-lg lg:text-xl rounded-full px-6 py-3 transition duration-300">
         Register Now to Get in Line
       </a>
     </div>
