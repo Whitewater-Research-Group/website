922b58cb12f1c04434c29880ed88b7a50434c380
obuhdaniel <danielobuh05@gmail.com>
2024-11-30 12:55:48 +0100

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import Home from './pages/home/Home'
-import EventsPage from './pages/events/Events'
-import ConferencePage from './pages/conference/Conference'
-import About from './pages/about/About'
-import ContactPage from './pages/contact/Contact'
-import PersonalizedInvitation from './components/Invitation'
+import Home from './pages/home/Home';
+import EventsPage from './pages/events/Events';
+import ConferencePage from './pages/conference/Conference';
+import About from './pages/about/About';
+import ContactPage from './pages/contact/Contact';
+import PersonalizedInvitation from './components/Invitation';
 
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -20,7 +20,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/letter" element={<PersonalizedInvitation />} />
-        {/* Redirect unknown routes to home instead of contact */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
